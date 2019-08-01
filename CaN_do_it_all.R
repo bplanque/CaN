6b1fa9a86daa3cf1aea092b7a827b5e3a79c870c
benjamin <benjamin.planque@imr.no>
2019-08-01 13:41:21 +0200

    Completed to matrix formulation of the dynamics and started the matrix formulation of constraints. Will stop there and write a new programme in which the dynamics of the trophic interactions and of the fisheries are all represented in the same matrix, ie. the fishery is represented as a species.

--- CaN_do_it_all.R
+++ CaN_do_it_all.R
@@ -34,13 +34,14 @@ require(expm)                                 # Loading the expm library -- requ
 # Biomass data - this must include estimates + min and max values (constraints)
 # Catches/landings - this must include estimates + min and max values (constraints)
 
-Biomasses=read.delim(file = 'Biomasses.csv',header = TRUE, sep=';')
-Years=Biomasses[,1]
-ny=length(Years)
-Species.names=colnames(Biomasses[seq(2,dim(Biomasses)[2],3)])
-ns=length(Species.names)
-Bio.min=Biomasses[,seq(3,dim(Biomasses)[2],3)]
-Bio.max=Biomasses[,seq(4,dim(Biomasses)[2],3)]
+Biomasses <- read.delim(file = 'Biomasses.csv',header = TRUE, sep=';')
+Years <- Biomasses[,1]
+ny <- length(Years)
+Species.names <- colnames(Biomasses[seq(2,dim(Biomasses)[2],3)])
+ns <- length(Species.names)
+Bio.min <- Biomasses[,seq(3,dim(Biomasses)[2],3)]
+Bio.max <- Biomasses[,seq(4,dim(Biomasses)[2],3)]
+B0 <- Biomasses[1,seq(2,dim(Biomasses)[2],3)]
 
 Landings=read.delim(file = 'Landings.csv',header = TRUE, sep=';')
 if(dim(Biomasses)[1]!=dim(Landings)[1]){
@@ -49,8 +50,8 @@ if(dim(Biomasses)[1]!=dim(Landings)[1]){
 if(dim(Biomasses)[2]!=dim(Landings)[2]){
   cat('not the same number of species in the biomass and landing datasets')
 }
-Land.min=Landings[,seq(3,dim(Biomasses)[2],3)]
-Land.max=Landings[,seq(4,dim(Biomasses)[2],3)]
+Land.min <- Landings[,seq(3,dim(Biomasses)[2],3)]
+Land.max <- Landings[,seq(4,dim(Biomasses)[2],3)]
 
 # loading input parameters ------------------------------------------------
 # This must include parameters on assimilation efficiency (gamma), 
@@ -62,62 +63,87 @@ if((dim(Input.params)[2]-1)!=ns){
   cat('not the same number of species in the biomass and parameters datasets')
 }
 
-Gamma=as.numeric(Input.params[1,2:(ns+1)])
-Kappa=as.numeric(Input.params[2,2:(ns+1)])
-Mu=as.numeric(Input.params[3,2:(ns+1)])
-Rho=as.numeric(Input.params[4,2:(ns+1)])
-Sigma=as.numeric(Input.params[5,2:(ns+1)])
+Gamma <- as.numeric(Input.params[1,2:(ns+1)])
+Kappa <- as.numeric(Input.params[2,2:(ns+1)])
+Mu <- as.numeric(Input.params[3,2:(ns+1)])
+Rho <- as.numeric(Input.params[4,2:(ns+1)])
+Sigma <- as.numeric(Input.params[5,2:(ns+1)])
 # alpha added by hand for now
-Alpha=c(100,100,100,0.25,100,100,0.5,0.5)
+Alpha <- c(100,100,100,0.25,100,100,0.5,0.5)
 
 Diets <- read.delim(file = 'Trophic.flows.csv',header = TRUE, sep = ';')
 # Construction of the diet matrix
 # Note: species interaction matrices are always constructed with prey in rows and predators in columns
-x1=data.frame(sp.num=1:ns,sp.name=Species.names)
-x2=merge(Diets,x1,by.x = "Prey", by.y = "sp.name",all.x = TRUE)
-x3=merge(x2,x1,by.x="Predator",by.y="sp.name",all.x=TRUE)
-DietMatrix=matrix(nrow = ns,ncol = ns,data = 0)
-DietMatrix[x3[,3]+(x3[,4]-1)*ns]=1
-DietVector=as.vector(DietMatrix)
+x1 <- data.frame(sp.num=1:ns,sp.name=Species.names)
+x2 <- merge(Diets,x1,by.x = "Prey", by.y = "sp.name",all.x = TRUE)
+x3 <- merge(x2,x1,by.x="Predator",by.y="sp.name",all.x=TRUE)
+DietMatrix <- matrix(nrow = ns,ncol = ns,data = 0)
+DietMatrix[x3[,3]+(x3[,4]-1)*ns] <- 1
+DietVector <- as.vector(DietMatrix)
 
 
 # deriving G, H and K -----------------------------------------------------
 # so that the Mater equation can be written in the form:
-# B(i,t+1) ??? B(i,t) = Sumj(G(j,i)F(j,i,t)???Sumj(K(i)F(i,j,t)) ??? H(i)B(i,t)
-H=1-exp(-Mu)
-K=H/Mu
-G=(Kappa%*%t(K*Gamma))*DietMatrix
-
+# B(i,t+1) − B(i,t) = Sumj(G(j,i)F(j,i,t)−Sumj(K(i)F(i,j,t)) − H(i)B(i,t)
+H <- 1-exp(-Mu)
+K <- H/Mu
+G <- (Kappa%*%t(K*Gamma))*DietMatrix
 
 # construction of the polytope --------------------------------------------
 # from the above, build matrix A and vector b so that A.f=<b, with f, the  
 # vector of all trophic flows at all time steps
 
-# Construction of matrix J
-Jijk=matrix(data=0,nrow = ns,ncol = ns^2)       # interaction betwee species i and flux j->k
-for (i in 1:ns){                                # loop on species (i)
-  deltaij=matrix(data=0,nrow = ns,ncol = ns)    # initialise matrix delta(i,j)
-  deltaij[i,]=1                                 # delta(i,j)=1 if the prey (row) is equal to i
-  deltaik=t(deltaij)                            # delta(i,k)=1 if the predator (col) is equal to i
-  Ji=G*deltaik-K[i]*deltaij                     # compute the sub-matrix J(j,k) for species i
-  Jijk[i,]=as.vector(Ji)                        # linearlise it and store it into the matrix J(i,jk)
+# matrix version of H (note (c) from equation 6)
+IE <- diag(ns)                                     # identity matrix of size E (number of species)
+H <- IE*H                                          # matrix with diagonal terms Hi 
+
+# Construction of matrix J (note (e) from equation 6)
+Jijk <- matrix(data=0,nrow = ns,ncol = ns^2)       # interaction betwee species i and flux j->k
+for (i in 1:ns){                                   # loop on species (i)
+  deltaij <- matrix(data=0,nrow = ns,ncol = ns)    # initialise matrix delta(i,j)
+  deltaij[i,] <- 1                                 # delta(i,j)=1 if the prey (row) is equal to i
+  deltaik <- t(deltaij)                            # delta(i,k)=1 if the predator (col) is equal to i
+  Ji <- G*deltaik-K[i]*deltaij                     # compute the sub-matrix J(j,k) for species i
+  Jijk[i,] <- as.vector(Ji)                        # linearlise it and store it into the matrix J(i,jk)
 }
 
-IE=diag(ns) # identity matrix of size E (number of species)
-L0=matrix(data = 0,nrow = ny,ncol = ny)        # initialise the L0 matrix (exponents required to compute L)
+# Matrix formulation of the dynamics (Appendix B.3)
+# Construct L, and M so that B=L.F+M (equation 14)
+
+# Construction of matrix L (equation 15)
+L0 <- matrix(data = 0,nrow = ny,ncol = ny)        # initialise the L0 matrix (exponents required to compute L)
 for (i in 1:(ny-1)){                            # loop on years
-  L0[(row(L0)-i)==col(L0)]<-i-1                 # assign exponent
+  L0[(row(L0)-i)==col(L0)] <- i-1                 # assign exponent
 }
-L=matrix(data=0,nrow = dim(Jijk)[1]*ny,ncol = dim(Jijk)[2]*ny) 
+L <- matrix(data=0,nrow = dim(Jijk)[1]*ny,ncol = dim(Jijk)[2]*ny) 
 for (j in 1:(ny-1)){                              # loop on columns (years)
   for (i in (j+1):ny){                          # loop on rows (years)
-    Lij=((IE-H)%^%L0[i,j])%*%Jijk                 # construct the submatrix L(i,j)
-    L[((i-1)*ns+1):(i*ns),((i-1)*(ns^2)+1):(i*(ns^2))]=Lij
+    Lij <- ((IE-H)%^%L0[i,j])%*%Jijk                 # construct the submatrix L(i,j)
+    L[((i-1)*ns+1):(i*ns),((i-1)*(ns^2)+1):(i*(ns^2))] <- Lij
   }
 }
 
-# Construction of matrix M
+# Construction of vector M (equation 16)
+M <- matrix(data=0,nrow = dim(Jijk)[1]*ny,ncol = 1)
+for (i in 1:ny){                          # loop on rows (years)
+  M[((i-1)*ns+1):(i*ns)] <- ((IE-H)%^%(i-1))%*%t(as.vector(B0))
+}
 
+# Matrix formulation of the constraints (Appendix B.4)
+# so that all constraints can be expressed in the form A.F<=B
+# constraint #1: flows are positive F>=0
+A1 <- diag(ns*ns*ny)*-1
+B1 <- rep(0,ns*ns*ny)
+A1 <- A1[rep(DietVector,ny),]; A1 <- A1[,rep(DietVector,ny)]
+# That looks ok but the problem is that the landings are not included in the flow matrices above
+
+# constraint #2: biomasses are positive L.F+M>=0
+# constraint #3: biomasses are bounded above: L.F+M<=Bio.max
+# constraint #4: biomasses are bounded below: L.F+M>=Bio.min
+# constraint #5: variations in biomass are bounded above: 
+# constraint #6: variations in biomass are bounded below: 
+# constraint #7: some flows (landings) are bouned above: F<=Land.max
+# constraint #8: some flows (landings) are bouned below: F>=Land.min
 
 # sampling of the polytope ------------------------------------------------
 
